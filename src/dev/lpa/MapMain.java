3bdedf955011975c368ede8c203fed67b8ce410d
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-07 20:27:17 +0200

    setup fulllist with email and phone contacts

--- src/dev/lpa/MapMain.java
+++ src/dev/lpa/MapMain.java
@@ -1,4 +1,18 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapMain {
+
+  public static void main(String[] args) {
+
+    List<Contact> phones = ContactData.getData("phone");
+    List<Contact> emails = ContactData.getData("email");
+
+    List<Contact> fullList = new ArrayList<>(phones);
+    fullList.addAll(emails);
+    fullList.forEach(System.out::println);
+    System.out.println("-------------------------------");
+  }
 }
